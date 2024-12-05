1bc1f58dce23e86dc6e1fc05e6ec0fb5bfc6859b
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-12-05 17:23:22 -0500

    Update conexion.php

--- CODIGO/bd/conexion.php
+++ CODIGO/bd/conexion.php
@@ -2,9 +2,9 @@
 class Conexion{	  
     public static function Conectar() {        
         define('servidor', 'localhost');
-        define('nombre_bd', 'usuarios_db');
-        define('usuario', 'root');
-        define('password', '');					        
+        define('nombre_bd', 'crud_2019');
+        define('usuario', 'seu_usuario');
+        define('password', 'sua_senha');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
             $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
